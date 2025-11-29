3a503923ff72e8e5cfdf5820bc2c0775cc8876b5
ktunprasert <ktunprasert@outlook.com>
2025-11-29 16:13:16 +0000

    two-fer: washed

--- two-fer/src/two_fer.clj
+++ two-fer/src/two_fer.clj
@@ -2,7 +2,5 @@
 
 (defn two-fer
   "Returns what you will say as you give away the extra cookie."
-  ([name]
-   (str "One for " (or name "you") ", one for me."))
-  ([]
-   "One for you, one for me."))
+  ([& [name]]
+   (str "One for " (or name "you") ", one for me.")))
